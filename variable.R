755c003e543dc567ae23c4009cd686fee80cfd53
Yuelin Zhou <47437563+yuelinzhou@users.noreply.github.com>
2021-12-23 04:36:52 -0800

    Create variable.R

---
+++ variable.R
@@ -0,0 +1,26 @@
+# ===================================================================
+# Title: variales
+# Description: this file is to test the variables by decleare in numbers
+# this file will not involve in app.r
+# Input(s): N/A
+# Output(s): N/A
+# Author: yuelinzhou
+# Date: 11-11-2019
+# ===================================================================
+
+# High Yield Savings Account
+rate_high <- 0.02 
+vol_high <- 0.001 
+
+
+# U.S. Bonds
+rate_bonds <- 0.05 
+vol_bonds <- 0.045 
+
+
+# U.S. Stocks
+rate_stocks <- 0.1
+vol_stocks <- 0.15
+
+# set seed
+set_seed <- 12345
